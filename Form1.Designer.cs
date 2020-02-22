ecc3bfe11b5a6ce43d33aa94b34184923401266d
annuciis <58049115+annuciis@users.noreply.github.com>
2020-02-22 19:49:33 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PacMan
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Hero = new System.Windows.Forms.PictureBox();
+            this.Food = new System.Windows.Forms.PictureBox();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.TimerMain = new System.Windows.Forms.Timer(this.components);
+            this.TimerPacman = new System.Windows.Forms.Timer(this.components);
+            this.TimerAnimateGhost = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Hero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Food)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Hero
+            // 
+            this.Hero.BackColor = System.Drawing.Color.Yellow;
+            this.Hero.Location = new System.Drawing.Point(375, 315);
+            this.Hero.Margin = new System.Windows.Forms.Padding(4);
+            this.Hero.Name = "Hero";
+            this.Hero.Size = new System.Drawing.Size(107, 98);
+            this.Hero.TabIndex = 0;
+            this.Hero.TabStop = false;
+            // 
+            // Food
+            // 
+            this.Food.BackColor = System.Drawing.Color.Lime;
+            this.Food.Location = new System.Drawing.Point(664, 223);
+            this.Food.Margin = new System.Windows.Forms.Padding(4);
+            this.Food.Name = "Food";
+            this.Food.Size = new System.Drawing.Size(60, 58);
+            this.Food.TabIndex = 1;
+            this.Food.TabStop = false;
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.ScoreLabel.Location = new System.Drawing.Point(16, 28);
+            this.ScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(178, 46);
+            this.ScoreLabel.TabIndex = 2;
+            this.ScoreLabel.Text = "Score: 0";
+            // 
+            // TimerMain
+            // 
+            this.TimerMain.Tick += new System.EventHandler(this.TimerMain_Tick);
+            // 
+            // TimerPacman
+            // 
+            this.TimerPacman.Tick += new System.EventHandler(this.TimerPacman_Tick);
+            // 
+            // TimerAnimateGhost
+            // 
+            this.TimerAnimateGhost.Tick += new System.EventHandler(this.TimerAnimateGhost_Tick);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 764);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.Food);
+            this.Controls.Add(this.Hero);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Game";
+            this.Text = "Form1";
+            
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Hero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Food)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Hero;
+        private System.Windows.Forms.PictureBox Food;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Timer TimerMain;
+        private System.Windows.Forms.Timer TimerPacman;
+        private System.Windows.Forms.Timer TimerAnimateGhost;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacMan
+{
+    public partial class Game : Form
+    {
+        private int verVelocity = 0;
+        private int horVelocity = 0;
+        private int pacmanImageCounter = 1;
+        private int enemyImageCounter = 1;
+        private string direction;
+        private bool gamePaused = false;
+
+        int heroSpeed = 1;
+        Random rand = new Random();
+        int score = 0;
+        public Game()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            InitializeHero();
+            InitilizeTimers();
+            PlaySound("pacman_intro.wav");
+
+        }
+        
+
+        private void InitilizeTimers()
+        {
+            TimerMain.Interval = 10;
+            TimerMain.Start();
+
+            TimerAnimateGhost.Interval = 300;
+            TimerAnimateGhost.Start();
+
+            TimerPacman.Interval = 100;
+            TimerPacman.Start();
+        }
+
+        private void AddGhost()
+        {
+            PictureBox ghost = new PictureBox();
+            ghost.Width = 20;
+            ghost.Height = 20;
+            ghost.Left = rand.Next(0, (ClientRectangle.Width - 50));
+            ghost.Top = rand.Next(0, (ClientRectangle.Height - 50));
+            ghost.SizeMode = PictureBoxSizeMode.StretchImage;
+            ghost.Image = Properties.Resources.enemy1_1;
+            ghost.BackColor = Color.Transparent;
+            ghost.Tag = "ghost";
+            this.Controls.Add(ghost);
+        }
+
+        private void InitializeHero()
+        {
+            Hero.BackColor = Color.Transparent;
+            Hero.Width = 40;
+            Hero.Height = 40;
+            Hero.SizeMode = PictureBoxSizeMode.StretchImage;
+            
+        }
+
+        private void HeroEnemyCollision()
+        {
+            foreach(Control contr in this.Controls)
+            {
+                if(contr.Tag == "ghost")
+                {
+                    if (Hero.Bounds.IntersectsWith(contr.Bounds))
+                    {
+                        GameOver();
+                    }
+                }
+            }
+        }
+
+        private void GameOver()
+        {
+            TimerMain.Stop();
+            TimerPacman.Stop();
+            PlaySound("game_over.wav");
+            MessageBox.Show("GAME OVER");
+            Application.Restart();
+            Environment.Exit(0);
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+                PlaySound("pacman_move.wav");
+
+            if (e.KeyCode == Keys.Up)
+            {
+                verVelocity = -heroSpeed;
+                horVelocity = 0;
+                direction = "up";
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                verVelocity = heroSpeed;
+                horVelocity = 0;
+                direction = "down";
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                verVelocity = 0;
+                horVelocity = -heroSpeed;
+                direction = "left";
+            }                
+            else if (e.KeyCode == Keys.Right)
+            {
+                verVelocity = 0;
+                horVelocity = heroSpeed;
+                direction = "right";
+            }
+            
+            else if (e.KeyCode == Keys.P)
+            {
+                if (gamePaused)
+                {
+                    TimerMain.Start();
+                    TimerPacman.Start();
+                    gamePaused = false;
+                    ScoreLabel.Text = "Score: " + score.ToString();
+                }
+                else
+                {
+                    TimerMain.Stop();
+                    TimerPacman.Stop();
+                    gamePaused = true;
+                    ScoreLabel.Text = "Game Paused...";
+                }                
+            }
+            else if (e.KeyCode == Keys.T)
+            {
+                SaySomething("Cool");
+            }
+            else if (e.KeyCode == Keys.C)
+            {
+                ShowCoordinates();
+            }
+        }
+
+        private void SaySomething(string word)
+        {
+            MessageBox.Show(word);
+        }
+
+        private void ShowCoordinates()
+        {
+            string message;
+            message = "Left=" + Hero.Left.ToString() + "; Top =" + Hero.Top.ToString();
+            SaySomething(message);
+            
+        }
+
+        private void PlaySound(string file)
+        {
+            string path = @"C:\Users\aannu\Downloads\Sounds\Sounds\" + file;
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(path);
+
+            try
+            {
+                player.Play();
+            }
+            catch(Exception e)
+            {
+                
+            }           
+        }
+
+        private void HeroMove()
+        {
+            
+            Hero.Top += verVelocity;
+            Hero.Left += horVelocity;
+            
+
+        }
+
+        private void HeroRectangleCollision()
+        {
+            if (Hero.Top < 0)
+                Hero.Top = ClientRectangle.Height;
+            if (Hero.Top > ClientRectangle.Height)
+                Hero.Top = 0;
+            if (Hero.Left < 0)
+                Hero.Left = ClientRectangle.Width;
+            if (Hero.Left > ClientRectangle.Width)
+                Hero.Left = 0;
+        }
+
+        private void TimerMain_Tick(object sender, EventArgs e)
+        {
+            
+            HeroMove();
+            HeroRectangleCollision();
+            HeroFoodCollision();
+            HeroEnemyCollision();
+        }
+
+        private void HeroFoodCollision()
+        {
+            if (Hero.Bounds.IntersectsWith(Food.Bounds))
+            {
+                Food.Top = rand.Next(0, (ClientRectangle.Height - 50));
+                Food.Left = rand.Next(0, (ClientRectangle.Width - 50));
+                score++; //score += 1;
+                ScoreLabel.Text = "Score: " + score.ToString();
+                heroSpeed += 1;
+                PlaySound("pacman_eat.wav");
+                AddGhost();
+            }
+        }
+
+        private void TimerPacman_Tick(object sender, EventArgs e)
+        {
+            string pacmanImageName = "pacman_" + pacmanImageCounter.ToString();
+            Hero.Image = (Image)Properties.Resources.ResourceManager.GetObject(pacmanImageName);
+            pacmanImageCounter += 1;
+            
+            if (pacmanImageCounter > 4) pacmanImageCounter = 1;
+
+            if (direction == "up")
+                Hero.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            else if (direction == "down")
+                Hero.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            else if(direction == "left")
+                Hero.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            else if(direction == "right")
+                Hero.Image.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+        }
+
+        private void TimerAnimateGhost_Tick(object sender, EventArgs e)
+        {
+            PictureBox ghost;
+            string enemyImageName = "enemy1_" + enemyImageCounter.ToString();
+            
+            foreach(Control contr in this.Controls)
+            {
+                if (contr.Tag == "ghost")
+                {
+                    ghost = (PictureBox)contr;
+                    ghost.Image = (Image)Properties.Resources.ResourceManager.GetObject(enemyImageName);
+                }
+            }
+            
+            enemyImageCounter += 1;
+            if (enemyImageCounter > 2) enemyImageCounter = 1;
+        }
+
+        
+    }
+}
